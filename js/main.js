b207f43590ee7e7e93b2633b391b209654acda3f
LaithAlOudat <aloudat.laith@gmail.com>
2021-03-18 07:33:13 +0200

    modifing some codes

--- js/main.js
+++ js/main.js
@@ -5,23 +5,23 @@ let userScore = 0;
 // Wellcoming message (asking about user name)
 let name1 = prompt('HELLO!.. Can you tell me your name?');
 console.log('User name is: ' + name1);
-alert('Hoody! .. nice to meet you ' + name1);
+alert('Hey! .. nice to meet you ' + name1);
 
 
 
 
 // function for 1st Q
-let age = prompt('Am I 30 years old ? y=yes/n=no');
+let age = prompt('Am I 30 Years Old? y=yes/n=no');
 function ageq1() {
   if ((age.toLocaleLowerCase() === 'y') || (age.toLocaleLowerCase() === 'yes')) {
-    console.log('Thats right');
-    alert('Thats right');
+    console.log('Thats right answer');
+    alert('Thats right!');
     userScore++;
   }
   else {
-    console.log('No No, wrong answer.');
+    console.log('wrong answer.');
 
-    alert('Wrong answer. My age is really 30!');
+    alert('No No! Wrong answer. My age is 30, really!!');
   }
 }
 ageq1();
@@ -45,8 +45,8 @@ function jobq2() {
     userScore++;
   }
   else {
-    console.log('No No, wrong answer. I love Shawermah');
-    alert('Wrong answer. love Shawermah');
+    console.log('wrong answer.');
+    alert('No No! Wrong answer. I am ... i guess! xD');
   }
 }
 jobq2();
@@ -63,7 +63,7 @@ else {
   alert('Wrong answer. love Shawermah');
 }
 */
-let field = prompt('Is my favorite field of scince is data scince ? y=yes/n=no');
+let field = prompt('Am I like Computers, Data science and, A.I. ? y=yes/n=no');
 
 
 // function for 3rd Q
@@ -75,8 +75,8 @@ function fieldq3() {
     userScore++;
   }
   else {
-    console.log('No No, wrong answer. I love Shawermah');
-    alert('Wrong answer. love Shawermah');
+    console.log('Wrong answer.');
+    alert('No No! Wrong answer.');
   }
 }
 fieldq3();
@@ -93,7 +93,7 @@ else {
   alert('Wrong answer. love Shawermah');
 }
 */
-let hoppy = prompt('Is one of my hoppy weight lefting fitness sport ? y=yes/n=no');
+let hoppy = prompt('Am I like weight lefting fitness sport? y=yes/n=no');
 
 // function for 4th Q
 
@@ -104,8 +104,8 @@ function hoppyq4() {
     userScore++;
   }
   else {
-    console.log('No No, wrong answer.');
-    alert('Wrong answer.');
+    console.log('Wrong answer.');
+    alert('No No! Wrong answer. I love WEIGHT LIFTING');
   }
 }
 hoppyq4();
@@ -122,7 +122,7 @@ else {
   alert('Wrong answer. love Shawermah');
 }
 */
-let food = prompt('Is my favorite food is shawermah? y=yes/n=no');
+let food = prompt('Is my favorite food is Shawarma? y=yes/n=no');
 
 // function for 5th Q
 
@@ -133,8 +133,8 @@ function foodq5() {
     userScore++;
   }
   else {
-    console.log('No No, wrong answer.');
-    alert('Wrong answer.');
+    console.log('Wrong answer.');
+    alert('No No! Wrong answer. I love Shawarma');
   }
 }
 foodq5();
@@ -160,14 +160,14 @@ function guessq6() {
   while (a > 0) {
     let guess = prompt('Guess the number that i hide (1-50)');
     if (guess === rightAnswer) {
-      alert('WoOoW. Thats Right!');
+      alert('WoOoW. Thats Right! ');
       // eslint-disable-next-line no-unused-vars
       userScore++;
       break;
     } else if (guess > rightAnswer) {
-      alert('too high');
+      alert('Unfortunately :( too high');
     } else {
-      alert('too low');
+      alert('Unfortunately :( too low');
     }
     a--;
     alert(a + ' shots left for you.');
@@ -198,30 +198,31 @@ while (a > 0) {
 console.log('The answer is: 35');
 */
 let a2 = 6;
-let favMovies = ['inception', 'interstellar', 'X-Men', 'Avengers'];
+let favMovies = ['Limitless', 'Interstellar', 'Ruhs Hour', 'The Martian'];
 
 // function for 7th Q
 
 function favMoviesq7() {
   while (a2 > 0) {
     let userAnswer = prompt('Can you guess one of my favorite movies?');
+    // eslint-disable-next-line no-undef
     if (userAnswer === favMovies[0]) {
-      alert('WoOoW! Nice one! right answer');
+      alert('WoOoW! Nice one! right answer.');
       // eslint-disable-next-line no-undef
       userScore++;
       break;
     } else if (userAnswer === favMovies[1]) {
-      alert('WoOoW! Nice one! right answer');
+      alert('WoOoW! Nice one! right answer.');
       // eslint-disable-next-line no-undef
       userScore++;
       break;
     } else if (userAnswer === favMovies[2]) {
-      alert('WoOoW! Nice one! right answer');
+      alert('WoOoW! Nice one! right answer.');
       // eslint-disable-next-line no-undef
       userScore++;
       break;
     } else if (userAnswer === favMovies[3]) {
-      alert('WoOoW! Nice one! right answer');
+      alert('WoOoW! Nice one! right answer.');
       // eslint-disable-next-line no-undef
       userScore++;
       break;
